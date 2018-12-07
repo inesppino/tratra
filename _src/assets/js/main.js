8ec8b91577d32083cb6c1224711131ba623e835d
inesppino <inesp.pino@gmail.com>
2018-12-07 19:50:12 +0100

    Put in screen the name

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-console.log('>> Ready :)');
+const input = document.querySelector('#user');
+const btn = document.querySelector('.search-btn');
+
+//Al hacer click en buscar, pedir a Github la información sobre la usuaria en su API
+
+function github(e){
+fetch('https://api.github.com/users/' + input.value)
+ .then(response => response.json())
+ .then(data => {
+   const result = document.querySelector('.resultingname');
+       result.innerHTML = data.name;
+
+       let sentence = data.name;
+
+       let gitname = sentence.split(" ", 1);
+       
+    //    let pieces = gitname.split("");
+       
+       console.log(gitname);
+ });
+}
+
+btn.addEventListener('click', github);
