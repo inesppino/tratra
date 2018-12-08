9c4f3d40538542d4afb15e9d129fb656670bb994
inesppino <inesp.pino@gmail.com>
2018-12-08 11:46:25 +0100

    print resulting name letter by letter on ordered list

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -6,29 +6,29 @@ const list = document.querySelector('.list-name');
 
 //Al hacer click en buscar, pedir a Github la información sobre la usuaria en su API
 
-function github(e){
-fetch('https://api.github.com/users/' + input.value)
- .then(response => response.json())
- .then(data => {
-   const result = document.querySelector('.resultingname');
+function github(e) {
+  fetch('https://api.github.com/users/' + input.value)
+    .then(response => response.json())
+    .then(data => {
+      const result = document.querySelector('.resultingname');
 
-        //Aqui del nombre y apellido te coge solo el nombre
-       let sentence = data.name;
+      //Aqui del nombre y apellido te coge solo el nombre
+      let sentence = data.name;
 
-       let fullName = sentence.split(" ");
-       console.log(fullName);
+      let fullName = sentence.split(" ");
+      console.log(fullName);
 
-       //Aqui donde dividimos el nombre y lo pintamos letra por letra
-       let gitname = fullName[0];
-       let pieces = gitname.split("");
-       console.log(pieces);
-    
-       let acc = 0;
-       for (let i = 0; i < pieces.length; i++) {
-         const letter = pieces[i];
-         console.log(`${letter}`);
-   }
- });
+      //Aqui donde dividimos el nombre y lo pintamos letra por letra
+      let gitname = fullName[0];
+      let pieces = gitname.split("");
+      console.log(pieces);
+
+      //Aqui pinta en una lista ordenada las letras del nombre
+      for (let i = 0; i < pieces.length; i++) {
+        const letter = pieces[i];
+        list.innerHTML += `<li>${[letter]}</li>`;
+      }
+    });
 }
 
 btn.addEventListener('click', github);
